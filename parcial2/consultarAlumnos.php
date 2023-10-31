088420c746e2782cb5203b5557cc150a004a7b88
Chav3zz <124410868+Chav3zz@users.noreply.github.com>
2023-10-30 19:31:08 -0700

    30/10/23

--- parcial2/consultarAlumnos.php
+++ parcial2/consultarAlumnos.php
@@ -44,8 +44,8 @@
                                         <td><?php echo $registro["turno"]; ?></td>
                                         <td><?php echo $registro["sexo"]; ?></td>
                                         <td>
-                                            <a href="" class="btn btn-xs btn-primary">Editar</a>
-                                            <a href="" class="btn btn-xs btn-danger">Eliminar</a>
+                                            <a href="editarAlumnos.php?id=<?php echo $registro["id"]?>" class="btn btn-xs btn-primary">Editar</a>
+                                            <a href="eliminarAlumno.php?id=<?php echo $registro["id"]?>" class="btn btn-xs btn-danger">Eliminar</a>
                                         </td>
                                     </tr>
                                 <?php }?>
